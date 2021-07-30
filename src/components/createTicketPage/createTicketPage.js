4d5745b58814544b901ca208ef9298a4458e6ae8
amirfakhrullah <amirfakhrullah96@gmail.com>
2021-07-30 22:46:26 +0800

    progress bar chart added, loading feature on createTicketPage added

--- src/components/createTicketPage/createTicketPage.js
+++ src/components/createTicketPage/createTicketPage.js
@@ -7,6 +7,8 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import * as organizationAction from '../../redux/actions/organizationAction';
 
+import Loading from '../loading/loading';
+
 import { Formik, Field, Form } from 'formik';
 import * as yup from 'yup';
 
@@ -17,7 +19,6 @@ const formSchema = yup.object({
 
 export default function CreateTicketPage({ match }) {
 
-    const history = useHistory();
     const dispatch = useDispatch();
 
     const [members, setMembers] = useState([]);
@@ -35,157 +36,167 @@ export default function CreateTicketPage({ match }) {
             })
     }, [dispatch, match.params.id]);
 
-    return (
-        <div className="createTicketPage">
-            <ArrowBackIcon onClick={() => window.location.href=`/organizations/${match.params.id}`} className="backIcon" style={{ fontSize: '30px', color: '#8481E2' }} />
-            <h1 style={{ margin: '0px 0px 20px 20px' }}>Open an issue / ticket</h1>
-            <div className="createTicketPage__container">
-                <Formik
-                    initialValues={{
-                        title: '',
-                        reference: '',
-                        description: '',
-                        assigned: '',
-                        status: '',
-                        priority: ''
-                    }}
-                    validationSchema={formSchema}
-                    onSubmit={(values) => {
-                        console.log({
-                            title: values.title,
-                            reference: values.reference,
-                            description: values.description,
-                            creator: {
-                                email: localStorage.getItem('useremail'),
-                                _id: localStorage.getItem('userid')
-                            },
-                            assigned: {
-                                email: values.assigned,
-                                _id: findMemberIdByEmail(members, values.assigned)
-                            },
-                            status: values.status,
-                            priority: values.priority
-                        });
-                    }}
-                >
-                    {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        isSubmitting,
-                        /* and other goodies */
-                    }) => (
-                        <form className="login-form" onSubmit={handleSubmit}>
+    const { loading } = useSelector(state => state.organization);
 
-                            <input
-                                className="ticket-input"
-                                type="text"
-                                name="title"
-                                placeholder='Issue Title *'
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.title}
-                            />
-                            <p className="form-error">{errors.title && touched.title && errors.title}</p>
+    var content;
+    if (loading === 'idle' || loading === 'loading') {
+        content = <Loading />
+    } else {
+        content = (
+            <div className="createTicketPage">
+                <ArrowBackIcon onClick={() => window.location.href = `/organizations/${match.params.id}`} className="backIcon" style={{ fontSize: '30px', color: '#8481E2' }} />
+                <h1 style={{ margin: '0px 0px 20px 20px' }}>Open an issue / ticket</h1>
+                <div className="createTicketPage__container">
+                    <Formik
+                        initialValues={{
+                            title: '',
+                            reference: '',
+                            description: '',
+                            assigned: '',
+                            status: '',
+                            priority: ''
+                        }}
+                        validationSchema={formSchema}
+                        onSubmit={(values) => {
+                            console.log({
+                                title: values.title,
+                                reference: values.reference,
+                                description: values.description,
+                                creator: {
+                                    email: localStorage.getItem('useremail'),
+                                    _id: localStorage.getItem('userid')
+                                },
+                                assigned: {
+                                    email: values.assigned,
+                                    _id: findMemberIdByEmail(members, values.assigned)
+                                },
+                                status: values.status,
+                                priority: values.priority
+                            });
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                            /* and other goodies */
+                        }) => (
+                            <form className="login-form" onSubmit={handleSubmit}>
 
-                            <input
-                                className="ticket-input"
-                                type="text"
-                                name="reference"
-                                placeholder='Issue Reference'
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.reference}
-                            />
-                            <p className="form-error"></p>
+                                <input
+                                    className="ticket-input"
+                                    type="text"
+                                    name="title"
+                                    placeholder='Issue Title *'
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.title}
+                                />
+                                <p className="form-error">{errors.title && touched.title && errors.title}</p>
 
-                            <textarea
-                                rows="7"
-                                className="ticket-input"
-                                type="text"
-                                name="description"
-                                placeholder='Description *'
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.description}
-                            />
-                            <p className="form-error">{errors.description && touched.description && errors.description}</p>
+                                <input
+                                    className="ticket-input"
+                                    type="text"
+                                    name="reference"
+                                    placeholder='Issue Reference'
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.reference}
+                                />
+                                <p className="form-error"></p>
 
-                            <Form>
-                                <label htmlFor="assigned">Assigned to :</label>
-                                <Field as="select" style={{
-                                    padding: '5px 10px',
-                                    margin: '10px'
-                                }} name="assigned">
-                                    <option value=''>None</option>
-                                    {
-                                        members && members.map(member => (
-                                            <option key={member._id} value={member.email}>{member.email}</option>
-                                        ))
-                                    }
-                                </Field>
+                                <textarea
+                                    rows="7"
+                                    className="ticket-input"
+                                    type="text"
+                                    name="description"
+                                    placeholder='Description *'
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.description}
+                                />
+                                <p className="form-error">{errors.description && touched.description && errors.description}</p>
 
-                                <div id="my-radio-group" style={{
-                                    margin: '15px 0px 5px 0px'
-                                }}>Status * :</div>
-                                <div role="group" aria-labelledby="my-radio-group" style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between'
-                                }}>
-                                    <label>
-                                        <Field type="radio" name="status" value="Not yet started" />
-                                        Not yet started
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="status" value="Ongoing" />
-                                        Ongoing
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="status" value="Stuck" />
-                                        Stuck
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="status" value="Done" />
-                                        Done
-                                    </label>
-                                </div>
+                                <Form>
+                                    <label htmlFor="assigned">Assigned to :</label>
+                                    <Field as="select" style={{
+                                        padding: '5px 10px',
+                                        margin: '10px',
+                                        backgroundColor: 'rgba(255, 255, 255, 0.6)'
+                                    }} name="assigned">
+                                        <option value=''>None</option>
+                                        {
+                                            members && members.map(member => (
+                                                <option key={member._id} value={member.email}>{member.email}</option>
+                                            ))
+                                        }
+                                    </Field>
 
-                                <div id="my-radio-group2" style={{
-                                    margin: '20px 0px 5px 0px'
-                                }}>Priority * :</div>
-                                <div role="group" aria-labelledby="my-radio-group" style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between'
-                                }}>
-                                    <label>
-                                        <Field type="radio" name="priority" value="No-Priority" />
-                                        No-Priority
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="priority" value="Low" />
-                                        Low
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="priority" value="Medium" />
-                                        Medium
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="priority" value="High" />
-                                        High
-                                    </label>
-                                </div>
-                            </Form>
+                                    <div id="my-radio-group" style={{
+                                        margin: '15px 0px 5px 0px'
+                                    }}>Status * :</div>
+                                    <div role="group" aria-labelledby="my-radio-group" style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between'
+                                    }}>
+                                        <label>
+                                            <Field type="radio" name="status" value="Not yet started" />
+                                            Not yet started
+                                        </label>
+                                        <label>
+                                            <Field type="radio" name="status" value="Ongoing" />
+                                            Ongoing
+                                        </label>
+                                        <label>
+                                            <Field type="radio" name="status" value="Stuck" />
+                                            Stuck
+                                        </label>
+                                        <label>
+                                            <Field type="radio" name="status" value="Done" />
+                                            Done
+                                        </label>
+                                    </div>
 
-                            <button className="form-button" type="submit" disabled={isSubmitting}>
-                                Create Ticket
-                            </button>
-                        </form>
-                    )}
-                </Formik>
+                                    <div id="my-radio-group2" style={{
+                                        margin: '20px 0px 5px 0px'
+                                    }}>Priority * :</div>
+                                    <div role="group" aria-labelledby="my-radio-group" style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between'
+                                    }}>
+                                        <label>
+                                            <Field type="radio" name="priority" value="No-Priority" />
+                                            No-Priority
+                                        </label>
+                                        <label>
+                                            <Field type="radio" name="priority" value="Low" />
+                                            Low
+                                        </label>
+                                        <label>
+                                            <Field type="radio" name="priority" value="Medium" />
+                                            Medium
+                                        </label>
+                                        <label>
+                                            <Field type="radio" name="priority" value="High" />
+                                            High
+                                        </label>
+                                    </div>
+                                </Form>
+
+                                <button className="form-button" type="submit" disabled={isSubmitting}>
+                                    Create Ticket
+                                </button>
+                            </form>
+                        )}
+                    </Formik>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+    return <React.Fragment>{content}</React.Fragment>
 }
--- src/components/donutChart/donutChart.js
+++ src/components/donutChart/donutChart.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './donutChart.css';
 
 import { ChartDonut } from '@patternfly/react-charts';
+import { BorderLeft } from '@material-ui/icons';
 
 export default function DonutChart(props) {
 
-    const countPriority = type => {
+    const countPriority = priority => {
         if (props.assignedTickets.result) {
-            return props.assignedTickets.result.filter(ticks => ticks.priority === type).length;
+            return props.assignedTickets.result.filter(ticks => ticks.priority === priority).length;
         } else {
             return 0;
         }
@@ -22,6 +23,22 @@ export default function DonutChart(props) {
         )
     }
 
+    // status pixel
+    const countStatusPixels = status => {
+        if (props.assignedTickets.result) {
+            return (props.assignedTickets.result.filter(ticks => ticks.status === status).length !== 0) ? ((props.assignedTickets.result.filter(ticks => ticks.status === status).length) / (props.assignedTickets.result.length) * 300) : 5
+        } else {
+            return 0;
+        }
+    }
+
+    const countStatusAmount = status => {
+        if (props.assignedTickets.result) {
+            return props.assignedTickets.result.filter(ticks => ticks.status === status).length;
+        } else {
+            return 0;
+        }
+    }
 
     const chartData = [
         {
@@ -49,15 +66,15 @@ export default function DonutChart(props) {
     return (
         <div className="donutChart">
             {/* Donut chart */}
-            <div style={{ height: '375px', width: '400px' }}>
+            <div style={{ width: '400px' }}>
                 <ChartDonut
                     constrainToVisibleArea={true}
                     data={chartData}
                     donutOrientation="top"
-                    height={275}
+                    height={240}
                     labels={({ datum }) => `${datum.x}: ${datum.y}%`}
                     padding={{
-                        bottom: 65, // Adjusted to accommodate legend
+                        bottom: 20, // Adjusted to accommodate legend
                         left: 20,
                         right: 20,
                         top: 20
@@ -65,12 +82,86 @@ export default function DonutChart(props) {
                     width={300}
                     colorScale={['rgba(214, 69, 65, 1)', '#FDDA0D', '#0E26B1', 'green']}
                 />
+                <div className="dataText">
+                    <h2 style={{ textDecoration: 'underline' }}>Task Priorities :</h2>
+                    {dataText('rgba(214, 69, 65, 1)', chartData[0].x, chartData[0].y)}
+                    {dataText('#FDDA0D', chartData[1].x, chartData[1].y)}
+                    {dataText('#0E26B1', chartData[2].x, chartData[2].y)}
+                    {dataText('green', chartData[3].x, chartData[3].y)}
+                </div>
             </div>
-            <div className="dataText">
-                {dataText('rgba(214, 69, 65, 1)', chartData[0].x, chartData[0].y)}
-                {dataText('#FDDA0D', chartData[1].x, chartData[1].y)}
-                {dataText('#0E26B1', chartData[2].x, chartData[2].y)}
-                {dataText('green', chartData[3].x, chartData[3].y)}
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                <div style={{
+                    display: 'flex',
+                    height: '300px',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'flex-end',
+                    marginBottom: '20px'
+                }}>
+
+                    <div style={{
+                        width: '5vw',
+                        margin: '5px',
+                        height: `${countStatusPixels('Assigned')}px`,
+                        backgroundColor: '#77cad9'
+                    }}></div>
+
+                    <div style={{
+                        width: '5vw',
+                        margin: '5px',
+                        height: `${countStatusPixels('Ongoing')}px`,
+                        backgroundColor: '#2c5f88'
+                    }}></div>
+
+                    <div style={{
+                        width: '5vw',
+                        margin: '5px',
+                        height: `${countStatusPixels('Stuck')}px`,
+                        backgroundColor: '#f87969'
+                    }}></div>
+
+                    <div style={{
+                        width: '5vw',
+                        margin: '5px',
+                        height: `${countStatusPixels('Done')}px`,
+                        backgroundColor: '#ced645'
+                    }}></div>
+                </div>
+                <div className="dataText" style={{
+                    alignItems: 'flex-end'
+                }}>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}>
+                        <h2 style={{ textDecoration: 'underline' }}>Current Progress :</h2>
+                    </div>
+                    <div>
+                        <div className="priority-circle" style={{ backgroundColor: '#77cad9' }}></div>
+                        <h3>{countStatusAmount('Assigned')} Assigned</h3>
+                    </div>
+
+                    <div>
+                        <div className="priority-circle" style={{ backgroundColor: '#2c5f88' }}></div>
+                        <h3>{countStatusAmount('Ongoing')} Ongoing</h3>
+                    </div>
+
+                    <div>
+                        <div className="priority-circle" style={{ backgroundColor: '#f87969' }}></div>
+                        <h3>{countStatusAmount('Stuck')} Stuck</h3>
+                    </div>
+
+                    <div>
+                        <div className="priority-circle" style={{ backgroundColor: '#ced645' }}></div>
+                        <h3>{countStatusAmount('Done')} Done</h3>
+                    </div>
+                </div>
             </div>
         </div >
     )
