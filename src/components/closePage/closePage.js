85e222db207fe98c0422e4cdc4e9e28ed12fffb6
amirfakhrullah <amirfakhrullah96@gmail.com>
2021-08-16 09:34:38 +0800

    text editing in closePage

--- src/components/closePage/closePage.js
+++ src/components/closePage/closePage.js
@@ -5,7 +5,7 @@ export default function ClosePage() {
     return (
         <div className="closePage">
             <div className="closeInfo__container">
-                <h1>Traill App web app is not mobile friendly</h1>
+                <h1>Please open this app in desktop.</h1>
                 <h1>Mobile App version will be available soon!</h1>
             </div>
         </div>
